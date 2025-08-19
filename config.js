94ad5ed52ccb1e437995031139cc9afd82dc9d4a
Alfredo Martínez Ibañez <alfredo@MacBook-Air-de-Alfredo.local>
2025-08-19 14:04:40 +0200

    starting supabase

---
+++ config.js
@@ -0,0 +1,4 @@
+// Configuración pública para el navegador (anon key)
+export const SUPABASE_URL = "https://veerdeexfyxndvkcswbo.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlZXJkZWV4Znl4bmR2a2Nzd2JvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MDI2MjIsImV4cCI6MjA3MTE3ODYyMn0.2JUaqWoG8eUFHNVxitj-URkNvjOlgAi0771vC_GhHCs";
+
